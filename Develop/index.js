a408de6d2b8d3eecd08d29cfc12123d2d33e4d6a
Rob V <rvans003@fiu.edu>
2021-03-11 18:34:10 -0500

    began building out index.js and input functions for inquirer to build team

--- Develop/index.js
+++ Develop/index.js
@@ -10,10 +10,43 @@ const idArray = [];
 
 function mainMenu() {
     
+    //function that takes user input and creates a manager object
     function assignManager() {
+        inquirer.prompt([
+            {
+                type : "input",
+                name : "managerName",
+                message : "What is the manager's name?",
+            },
+            {
+                type : "input",
+                name : "managerId",
+                message : "What is the manager's ID?"
+            },
+            {
+                type : "input",
+                name : "managerEmail",
+                message : "What is the manager's email?"
+            },
+            {
+                type : "input",
+                name : "managerPhone",
+                message : "What is the manager's phone number?"
+            }
+        ]).then(response => {
+
+            //takes the user response and pushes the responses to the team members Array
+            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerPhone);
+            teamMembers.push(manager);
+            
+            //also pushes the id number to the id Array
+            idArray.push(response.managerId)
+            createTeam();
+        })
 
     }
 
+    //function to choose which type of entry user will be inputting
     function createTeam() {
         inquirer.prompt([
             {
@@ -27,16 +60,19 @@ function mainMenu() {
                     "I'm done adding team members!"
                 ]
             }
-        ]).then(userChoice =>{
-            switch (userChoice.teamMemberChoice) {
+        ]).then(choice =>{
+            //switch statement to check the user's choice and 
+            switch (choice.teamMemberChoice) {
+                
+                //the Engineer and Intern choice run the code to add the selected choice
                 case "Engineer":
                     addEngineer();
                     break;
                 case "Intern":
                     addIntern();
                     break;
-                case "I'm done adding team members!":
-                    buildTeam();
+
+                //default case to be choosen if user does not choose Engineer or Intern      
                 default : 
                     buildTeam();        
             }
@@ -45,6 +81,25 @@ function mainMenu() {
     }
 
     function addEngineer () {
+
+        inquirer.prompt([
+            {
+                type : "input",
+                name : "inputName",
+                message : "What is the engineer's name?"
+            },
+            {
+                type : "input",
+                name : "inputId",
+                message : "What is the engineer's ID number?"
+            },
+            {
+                type : "input",
+                name : "inputEmail",
+                message : "What is the engineer's email?"
+            }
+        ])
+
 
     }
 
