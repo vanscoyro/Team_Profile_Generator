c2a3f9c9b9c81757e87f825db662bd6c7750e4ca
Rob V <rvans003@fiu.edu>
2021-03-11 22:27:24 -0500

    fixed syntax error

--- Main/index.js
+++ Main/index.js
@@ -1,7 +1,7 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const inquirer = required("inquirer");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
